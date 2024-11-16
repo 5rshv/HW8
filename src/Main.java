e2f7dea00ebcfa8e04165d1deb6a84787f279f36
5rshv <serishev.alesha@gmail.com>
2024-11-16 15:44:17 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void calculateSum(String[] args) {
+
+    }
+
+    public static void checkMonth(String[] args) {
+
+    }
+
+    public static void printName(String[] args) {
+
+    }
+}
